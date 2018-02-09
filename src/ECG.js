a35417aa2f6e39447be19f501e77782a7c571490
latifa <latifa.mahmoud13@gmail.com>
2018-02-09 20:30:58 +0200

    first

---
+++ src/ECG.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import * as ReactD3 from "react-d3-components";
+import * as  Rebase from 're-base';
+import * as firebase from 'firebase';
+
+var app = firebase.initializeApp({
+  apiKey: "AIzaSyDhRCEQxql-BmnCvZUhHEQklrYzcDlEJbQ",
+  authDomain: "ecg-au-45a8c.firebaseapp.com",
+  databaseURL: "https://ecg-au-45a8c.firebaseio.com",
+  messagingSenderId: "659744702608"
+});
+
+var base = Rebase.createClass(app.database());
+
+var LineChart = ReactD3.LineChart;
+
+var data ={
+    values: [
+      { x: 0, y: 2 }, 
+      { x: 1.3, y: 5 },
+      { x: 3, y: 6 }, 
+      { x: 3.5, y: 6.5 }
+      ]
+  };
+
+var ecgData = null;
+
+class ECG extends Component {
+
+  render() {
+    base.fetch('0000ffe1-0000-1000-8000-00805f9b34fb', {
+      context: this,
+      asArray: true,
+      then(returnedData){
+        ecgData = returnedData;
+        console.log("ECG dtata is: ", ecgData);
+        data.values.push({x: 34, y: 440});
+        console.log("dtata is: ", data.values);
+
+      }
+    });
+
+    
+
+    return (
+      <div className="App">
+       <h2 style={{marginTop:"100px"}}>Some Readings example</h2>
+        <LineChart
+          data={data}
+          width={600}
+          height={600}
+          margin={{ top: 10, bottom: 50, left: 50, right: 10 }} />
+      </div>
+    );
+  }
+}
+
+export default ECG;
---
+++ src/Emergency.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem, Image } from 'react-bootstrap';
+
+class Emergency extends Component {
+  render() {
+    return (
+      <div className="App2">
+          <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+             <Navbar.Brand>
+                 <a href="#brand"></a>
+             </Navbar.Brand>
+             <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">
+                   Emergencies
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                   Patient's Info
+                </NavItem>
+                <NavDropdown eventKey={3} title="Options" id="basic-nav-dropdown">
+                   <MenuItem eventKey={3.1}>Action</MenuItem>
+                   <MenuItem eventKey={3.2}>Another action</MenuItem>
+                   <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                   <MenuItem divider />
+                   <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                </NavDropdown>
+              </Nav>
+              <Nav pullRight>
+                 <NavItem eventKey={1} href="#">
+                    Link Right
+                 </NavItem>
+                 <NavItem eventKey={2} href="#">
+                    Link Right
+                 </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>  
+          <h1 className="emer">Emergencies:</h1>
+          <div className="EmergencyCard">
+             <Image src="Patient1.jpeg" responsive/>
+              Patient's name<br/>
+              String of Condition<br/>
+              Location Emergency Time
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Emergency;
