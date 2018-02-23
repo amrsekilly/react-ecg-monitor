05c29fcccc734afa9507320314064fe8149f31cc
Amr Elsekilly <amr.e.dev@gmail.com>
2018-02-23 18:15:46 +0200

    last (yeah look at that commit message)

--- src/ECG.js
+++ src/ECG.js
@@ -16,57 +16,61 @@ var base = Rebase.createClass(app.database());
 
 var LineChart = ReactD3.LineChart;
 
-var data ={
-    values: [
-      { x: 0, y: 0 }
-      ]
-  };
+var data = {
+  values: [
+    { x: 0, y: 0 }
+  ]
+};
+var samplesCounter = 0;
 
 class ECG extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        values: [
+          { x: 0, y: 0 }
+        ]} };
+  }
+
   componentDidMount() {
-    setInterval(() => this.forceUpdate(), 1000);
-
-    // get initial data 
-    base.fetch('0000ffe1-0000-1000-8000-00805f9b34fb', {
-      context: this,
-      asArray: true,
-      then(returnedData) {
-        var ecgData = {};
-        ecgData.x = 0;
-        // console.log("ECG dtata is: ", returnedData);
-        for (const array of returnedData) {
-          // console.log(array);
-          for (let reading of array) {
-            // console.log(reading);
-            ecgData.y = reading;
-            ecgData.x++;
-            data.values.push(ecgData);
-            // console.log("Initial data is: ", data.values);
-          }
-        }
-        
-      }
-    });
-
-    // get updated data
+  //  get the data from firebase's real-time database
     base.listenTo('0000ffe1-0000-1000-8000-00805f9b34fb', {
       context: this,
       asArray: true,
+      queries: {
+        limitToLast: 1
+      },
       then(returnedData) {
-        var ecgData = {};
-        ecgData.x = 0;
-        // console.log("ECG dtata is: ", returnedData);
+        console.log("length of returned data: ", returnedData);
         for (const array of returnedData) {
-          // console.log(array);
+          // loop over all the returned arrays
           for (let reading of array) {
-            // console.log(reading);
-            ecgData.y = reading;
-            ecgData.x++;
+            // to hold the new readings object
+            if (reading > 5) {
+              continue;
+            }
+            let ecgData = {
+              x: samplesCounter++,
+              y: reading
+            };
+            // push the new ECG values to the values state
             data.values.push(ecgData);
           }
-        }
-        // console.log("Updated data is: ", data.values);
+        } // end of for 
+
+        this.setState({
+          data: [
+            data
+          ]
+        });
+        samplesCounter = 0;
+        data = {
+          values: [
+            { x: 0, y: 0 }
+          ]
+        };
       }
     });
 
@@ -78,8 +82,8 @@ class ECG extends Component {
       <div className="App">
        <h2 style={{marginTop:"100px"}}>ECG</h2>
         <LineChart
-          data={data}
-          width={600}
+          data={this.state.data}
+          width={1200}
           height={600}
           margin={{ top: 10, bottom: 50, left: 50, right: 10 }} />
       </div>
